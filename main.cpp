d613a799a1177581a714bba747c28f1865ce0836
Shohruh Rahmatillayev <rshohrux16@gmail.com>
2024-12-02 11:45:43 +0500

    day 2 2

--- main.cpp
+++ main.cpp
@@ -17,22 +17,30 @@ void t_main(){
     string line;
     while(getline(cin, line) && !line.empty()){
         stringstream ss(line);
-        vector<int> v;
+        vector<int> nums;
         int num;
-        int mn_dif = INT_MAX, mx_dif = INT_MIN;
         while(ss >> num){
-            v.push_back(num);
+            nums.push_back(num);
         }
-        for(int i = 1; i < v.size(); ++i){
-            mn_dif = min(mn_dif, v[i] - v[i-1]);
-            mx_dif = max(mx_dif, v[i] - v[i-1]);
+        int ok = 0;
+        for(int t = 0; t < nums.size(); ++t){
+            int mn_dif = INT_MAX, mx_dif = INT_MIN;
+            int elem = nums[t];
+            nums.erase(nums.begin() + t);
+            for(int i = 1; i < nums.size(); ++i){
+                mn_dif = min(mn_dif, nums[i] - nums[i-1]);
+                mx_dif = max(mx_dif, nums[i] - nums[i-1]);
+            }
+            if(mn_dif < 0) {
+                swap(mn_dif, mx_dif);
+                mn_dif = -mn_dif;
+                mx_dif = -mx_dif;
+            }
+            if(mn_dif >= 1 && mx_dif <= 3 && mn_dif <= mx_dif) ok |= 1;
+
+            nums.insert(nums.begin() + t, elem);
         }
-        if(mn_dif < 0) {
-            swap(mn_dif, mx_dif);
-            mn_dif = mn_dif;
-            mx_dif = mx_dif;
-        }
-        if(mn_dif >= 1 && mx_dif <= 3 && mn_dif <= mx_dif) ans++;
+        ans += ok;
     }
     cout << ans;
 }
