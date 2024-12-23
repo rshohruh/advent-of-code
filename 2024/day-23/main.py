0ae4d8624e20bc7fa40487a6e053fbb50b1f19ba
= <=>
2024-12-23 11:31:58 +0500

    day 23 without external library

--- 2024/day-23/main.py
+++ 2024/day-23/main.py
@@ -1,12 +1,53 @@
 import sys
-import networkx as nx  # pip install networkx
+adj = [tuple(str(x).split('-')) for x in sys.stdin.read().strip().split()]
 
-G = nx.Graph()
-G.add_edges_from(tuple(str(x).split('-')) for x in sys.stdin.read().strip().split())
+def bron_kerbosch(R, P, X, graph, cliques):
+    if not P and not X:
+        cliques.append(R)
+        return
+    for vertex in list(P):
+        bron_kerbosch(
+            R.union([vertex]),
+            P.intersection(graph[vertex]),
+            X.intersection(graph[vertex]),
+            graph,
+            cliques
+        )
+        P.remove(vertex)
+        X.add(vertex)
 
+def enumerate_all_cliques(graph):
+    def bron_kerbosch_all(R, P, X, graph, all_cliques):
+        all_cliques.append(R)
+        for vertex in list(P):
+            bron_kerbosch_all(
+                R.union([vertex]),
+                P.intersection(graph[vertex]),
+                X.intersection(graph[vertex]),
+                graph,
+                all_cliques
+            )
+            P.remove(vertex)
+            X.add(vertex)
+    
+    all_cliques = []
+    bron_kerbosch_all(set(), set(graph.keys()), set(), graph, all_cliques)
+    return all_cliques
+
+graph = {}
+for (u, v) in adj:
+    if u not in graph:
+        graph[u] = set()
+    if v not in graph:
+        graph[v] = set()
+    graph[u].add(v)
+    graph[v].add(u)
+
+cliques = enumerate_all_cliques(graph)
+# print(*cliques, sep='\n')
 
 # part one
-triangles = [clique for clique in nx.enumerate_all_cliques(G) if len(clique) == 3]
+triangles = [clique for clique in cliques if len(clique) == 3]
 ans = 0
 for clique in triangles:
     for u in clique:
@@ -14,9 +55,9 @@ for clique in triangles:
             ans += 1
             break
 print(ans)
-#part two
-cliques = list(nx.find_cliques(G))
-max_clique = max(cliques, key=len)
+
+# part two
+max_clique = list(max(cliques, key=len))
 max_clique.sort()
 
 print(",".join(max_clique))
